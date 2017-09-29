6ce2ad407a561b84b4fc9350dca422c24c797485
Unknown <zacharyd1@live.com>
2017-09-29 12:20:53 -0400

    Completed Exercise 2

--- Lab 5/Lab 5/pressure.cpp
+++ Lab 5/Lab 5/pressure.cpp
@@ -3,14 +3,18 @@
 
 #include <iostream>
 using namespace std;
- 
-main ()
+
+int main()
 {
-  int  pressure;
+	int  pressure;
+	else cout << "Everything seems normal." << endl;
 
   cout << "Enter an integer pressure reading. "
        << " Press Return."  << endl;
   cin >> pressure;
   if (pressure > 100) cout << "Warning!! Pressure reading above danger limit." << endl;
   return 0;
+
+	cin.get(); cin.get();	//Hold the window open.
+	return 0;
 }
