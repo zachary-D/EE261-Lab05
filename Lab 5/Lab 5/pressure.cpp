8fa39eb752ea16c05844276dc03750e27c18fffa
Unknown <zacharyd1@live.com>
2017-09-29 12:06:28 -0400

    Created solution, added given files

---
+++ Lab 5/Lab 5/pressure.cpp
@@ -0,0 +1,16 @@
+// Program Pressureprints appropriate messages based on
+// a pressure reading input from the keyboard.
+
+#include <iostream>
+using namespace std;
+ 
+main ()
+{
+  int  pressure;
+
+  cout << "Enter an integer pressure reading. "
+       << " Press Return."  << endl;
+  cin >> pressure;
+  /* FILL IN Code appropriate to exercise */ 
+  return 0;
+}
